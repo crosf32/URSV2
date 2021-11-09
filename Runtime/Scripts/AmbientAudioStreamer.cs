f130af8c903caa1c2ed59f95787eb4561b566965
crosf32 <golouis.72@gmail.com>
2021-11-09 09:13:51 +0100

    Add AmbientAudioStreamer.cs

---
+++ Runtime/Scripts/AmbientAudioStreamer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Unity.WebRTC;
+using UnityEngine;
+
+namespace Unity.RenderStreaming
+{
+    public class AmbienAudioStreamer : AudioStreamBase
+    {
+        private MediaStream m_audioStream;
+
+        void OnDisable()
+        {
+            WebRTC.Audio.Stop();
+        }
+
+        protected override MediaStreamTrack CreateTrack()
+        {
+            m_audioStream = Unity.WebRTC.Audio.CaptureStream();
+            return m_audioStream.GetTracks().First();
+        }
+
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            WebRTC.Audio.Update(data, channels);
+        }
+    }
+}

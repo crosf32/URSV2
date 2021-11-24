3903ba282105bb1f8124e6b9ce234fe0c660432a
crosf32 <golouis.72@gmail.com>
2021-11-24 11:30:36 +0100

    test

---
+++ Runtime/Scripts/AmbientStreamer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Unity.WebRTC;
+using UnityEngine;
+
+namespace Unity.RenderStreaming
+{
+    public class AmbientStreamer : AudioStreamBase
+    {
+        private MediaStream m_audioStream;
+
+        void OnDisable()
+        {
+            WebRTC.Audio.Stop();
+        }
+
+        protected override MediaStreamTrack CreateTrack()
+        {
+            m_audioStream = Unity.WebRTC.Audio.CaptureStream();
+            return m_audioStream.GetTracks().First();
+        }
+
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            WebRTC.Audio.Update(data, channels);
+        }
+    }
+}

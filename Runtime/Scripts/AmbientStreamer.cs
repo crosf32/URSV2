485d55b2a40a10c0577a4b0550f8961894d1bb71
crosf32 <golouis.72@gmail.com>
2021-11-24 14:38:41 +0100

    Fix AmbientStreamer.cs

--- Runtime/Scripts/AmbientStreamer.cs
+++ Runtime/Scripts/AmbientStreamer.cs
@@ -6,9 +6,32 @@ namespace Unity.RenderStreaming
 {
     public class AmbientStreamer : AudioStreamBase
     {
+        [SerializeField, Tooltip("AudioSource")]
+        private AudioSource audioSource;
+
+        protected virtual void OnEnable()
+        {
+            if (audioSource == null)
+            {
+                Debug.LogFormat("AudioSource required");
+                return;
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (audioSource == null)
+            {
+                return;
+            }
+
+            audioSource.Stop();
+            //audioSource.clip = null;
+        }
+
         protected override MediaStreamTrack CreateTrack()
         {
-            return new AudioStreamTrack();
+            return new AudioStreamTrack(audioSource);
         }
     }
 }
